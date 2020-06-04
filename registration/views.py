dffd4dfe36aec871feaa136c93b9b5a616f99ac8
cris <cris_rockanrolla2299@hotmail.com>
2020-06-04 13:47:24 -0500

    Email obligatorio al registrarse

--- registration/views.py
+++ registration/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserCreationFormWithEmail
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
 
 # Create your views here.
 class SignUpView(CreateView):
-    form_class = UserCreationForm
+    form_class = UserCreationFormWithEmail
     template_name = "registration/signup.html"
 
     def get_success_url(self):
