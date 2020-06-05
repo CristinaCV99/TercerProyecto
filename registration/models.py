56297da4454deed21289d24f09f0e4f5b7ff308e
cris <cris_rockanrolla2299@hotmail.com>
2020-06-04 23:51:28 -0500

    Primeras Pruebas

--- registration/models.py
+++ registration/models.py
@@ -23,6 +23,6 @@ class Profile(models.Model):
 def ensure_profile_exists(sender, instance, **kwargs):
     if kwargs.get('created', False):
         Profile.objects.get_or_create(user=instance)
-        print("Se acaba de crear un usuario y su perfil enlazado")
+        #print("Se acaba de crear un usuario y su perfil enlazado")
 
 
