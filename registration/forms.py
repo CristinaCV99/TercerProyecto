42ca4a2b76a620e813d781ede2e868445ab47b26
cris <cris_rockanrolla2299@hotmail.com>
2020-06-04 20:46:09 -0500

    Opcion de editar email

--- registration/forms.py
+++ registration/forms.py
@@ -1,19 +1,20 @@
+  
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
 class UserCreationFormWithEmail(UserCreationForm):
-    email = forms.EmailField(required=True, help_text="Requerido, 254 caracteres como máximo y debe ser valido")
+    email = forms.EmailField(required=True, help_text="Requerido. 254 carácteres como máximo y debe ser válido.")
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2" )
+        fields = ("username", "email", "password1", "password2")
 
     def clean_email(self):
         email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("El email ya existe, prueba con otro.")
+            raise forms.ValidationError("El email ya está registrado, prueba con otro.")
         return email
 
 
@@ -23,8 +24,21 @@ class ProfileForm(forms.ModelForm):
         fields = ['avatar', 'bio', 'link']
         widgets = {
             'avatar': forms.ClearableFileInput(attrs={'class':'form-control-file mt-3'}),
-            'bio': forms.Textarea(attrs={'class':'form-control-file mt-3', 'rows':3, 'placeholder':'Biografia'}),
-            'link': forms.URLInput(attrs={'class':'form-control-file mt-3', 'placeholder':'Enlace'}),
-
-
+            'bio': forms.Textarea(attrs={'class':'form-control mt-3', 'rows':3, 'placeholder':'Biografía'}),
+            'link': forms.URLInput(attrs={'class':'form-control mt-3', 'placeholder':'Enlace'}),
         }
+
+
+class EmailForm(forms.ModelForm):
+    email = forms.EmailField(required=True, help_text="Requerido. 254 carácteres como máximo y debe ser válido.")
+
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if 'email' in self.changed_data:
+            if User.objects.filter(email=email).exists():
+                raise forms.ValidationError("El email ya está registrado, prueba con otro.")
+        return email
--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from .forms import UserCreationFormWithEmail, ProfileForm
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.views.generic.edit import UpdateView
 from django.utils.decorators import method_decorator
@@ -25,6 +25,8 @@ class SignUpView(CreateView):
         form.fields['password2'].widget = forms.PasswordInput(
             attrs={'class':'form-control mb-2', 'placeholder':'Repite la contraseña'})  
         return form
+
+
 @method_decorator(login_required, name='dispatch')
 class ProfileUpdate(UpdateView):
     form_class = ProfileForm
@@ -35,4 +37,21 @@ class ProfileUpdate(UpdateView):
         #Recuperar el objeto que se va a editar
         profile, created = Profile.objects.get_or_create(user=self.request.user)
         return profile
+
+@method_decorator(login_required, name='dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('profile')
+    template_name = 'registration/profile_email.html'
+
+    def get_object(self):
+        #Recuperar el objeto que se va a editar
+        return self.request.user
+
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate,self).get_form()
+        #Modificar en tiempo real
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class':'form-control mb-2', 'placeholder':'Email'})
+        return form
 
